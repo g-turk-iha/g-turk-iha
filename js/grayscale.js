09d09109da0b07772560f2f778825f4f9adad31e
Muhammet Koc <mhmmtkc94@gmail.com>
2021-12-11 00:53:53 +0300

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -35,17 +35,4 @@ $("a").mouseup(function(){
     $(this).blur();
 })
 
-function onScroll (evt) {
 
-  // Store the scroll value for laterz.
-  lastScrollY = window.scrollY;
-
-  // Prevent multiple rAF callbacks.
-  if (scheduledAnimationFrame)
-    return;
-
-  scheduledAnimationFrame = true;
-  requestAnimationFrame(readAndUpdatePage);
-}
-
-window.addEventListener('scroll', onScroll);
